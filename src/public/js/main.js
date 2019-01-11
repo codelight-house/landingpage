843e39eefd279000f63d669357152f73094a9454
pozga <pablo.ozga@gmail.com>
2019-01-11 13:24:37 +0100

    vs workspace(for live reload server) and cookie bar

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -8,9 +8,9 @@
     "use strict";
 
     var cfg = {
-        scrollDuration: 800, // smoothscroll duration
-        mailChimpURL: 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
-    },
+            scrollDuration: 800, // smoothscroll duration
+            mailChimpURL: 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc' // mailchimp url
+        },
 
         $WIN = $(window);
 
@@ -18,31 +18,6 @@
     // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
-
-
-    /* Preloader
-     * -------------------------------------------------- */
-    var clPreloader = function () {
-
-        $("html").addClass('cl-preload');
-
-        $WIN.on('load', function () {
-
-            //force page scroll position to top at page refresh
-            // $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation
-            $("#loader").fadeOut("slow", function () {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-
-            // for hero content animations
-            $("html").removeClass('cl-preload');
-            $("html").addClass('cl-loaded');
-
-        });
-    };
 
 
     /* Menu on Scrolldown
@@ -55,8 +30,7 @@
 
             if ($WIN.scrollTop() > 150) {
                 menuTrigger.addClass('opaque');
-            }
-            else {
+            } else {
                 menuTrigger.removeClass('opaque');
             }
 
@@ -168,7 +142,11 @@
                     stats.each(function () {
                         var $this = $(this);
 
-                        $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                        $({
+                            Counter: 0
+                        }).animate({
+                            Counter: $this.text()
+                        }, {
                             duration: 4000,
                             easing: 'swing',
                             step: function (curValue) {
@@ -218,8 +196,7 @@
             //autoplay: true,
             pauseOnFocus: false,
             autoplaySpeed: 1000,
-            responsive: [
-                {
+            responsive: [{
                     breakpoint: 1200,
                     settings: {
                         slidesToShow: 5
@@ -258,8 +235,7 @@
             adaptiveHeight: true,
             pauseOnFocus: false,
             autoplaySpeed: 1500,
-            responsive: [
-                {
+            responsive: [{
                     breakpoint: 900,
                     settings: {
                         slidesToShow: 1,
@@ -430,10 +406,10 @@
      * ------------------------------------------------------ */
     var clBackToTop = function () {
 
-        var pxShow = 500,         // height on which the button will show
-            fadeInTime = 400,         // how slow/fast you want the button to show
-            fadeOutTime = 400,         // how slow/fast you want the button to hide
-            scrollSpeed = 300,         // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+        var pxShow = 500, // height on which the button will show
+            fadeInTime = 400, // how slow/fast you want the button to show
+            fadeOutTime = 400, // how slow/fast you want the button to hide
+            scrollSpeed = 300, // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
             goTopButton = $(".go-top")
 
         // Show or hide the sticky footer button
@@ -451,7 +427,6 @@
      * ------------------------------------------------------ */
     (function ssInit() {
 
-        clPreloader();
         clMenuOnScrolldown();
         clOffCanvas();
         clPhotoswipe();
